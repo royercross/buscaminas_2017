a5e08baadc5e64d6dc88ba9f707772d8e43fde9b
Rogelio Noris <rogelionoris@royercross-mbp.local>
2017-02-14 10:11:42 -0700

    * Primer commit

---
+++ PA2017_Buscaminas/src/pa2017_buscaminas/PA2017_Buscaminas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pa2017_buscaminas;
+
+/**
+ *
+ * @author rogelionoris
+ */
+public class PA2017_Buscaminas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Buscaminas ventana = new Buscaminas();
+        ventana.setVisible(true);
+        
+    }
+    
+}
